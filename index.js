304f7bcefaa186bd0ff4068d539d31a52a19c875
RushWTF <byrushcoc@gmail.com>
2022-07-06 17:52:07 +0200

    Electron adelanto

--- index.js
+++ index.js
@@ -78,6 +78,49 @@ console.log();
 
 
 
+//Electron
+const {app, BrowserWindow} = require('electron')
+  
+  const winsettings = {
+    width: 300,
+    height: 500,
+    icon: __dirname + "/assets/league of legends.ico",
+    webPreferences: {
+      devTools: true,
+      nodeIntegration: true,
+    },
+    autoHideMenuBar: true,
+    frame: false,
+    resizable: false,
+    maximizable: false,
+    fullscreenable: false
+  }
+  
+  const createWindow = () => {
+    const win = new BrowserWindow(winsettings)
+  
+    win.loadFile('./app/index.html')
+  }
+  
+  
+  app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
+  })
+  
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+
+
 
 const notifier = require('node-notifier');
 const path = require('path');
